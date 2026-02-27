f32357f8663c5da5629868e2a133c29b2ec12858
Roger Welin <roger.welin@icloud.com>
2026-02-27 15:48:21 +0100

    test input data

--- pg_fk_indexer--1.0.sql
+++ pg_fk_indexer--1.0.sql
@@ -1,4 +1,4 @@
-CREATE FUNCTION pg_fk_indexer_hello()
-RETURNS text
-AS 'MODULE_PATHNAME', 'pg_fk_indexer_hello'
+CREATE FUNCTION pg_fk_indexer_add(a int4, b int4)
+RETURNS int4
+AS 'MODULE_PATHNAME', 'pg_fk_indexer_add'
 LANGUAGE C IMMUTABLE STRICT;
--- pg_fk_indexer.c
+++ pg_fk_indexer.c
@@ -2,17 +2,13 @@
 #include "fmgr.h"
 #include "utils/builtins.h"
 
-// postgres.h --> postgres core definitions; types, macros, server internals etc
-// fmgr.h --> pulls in the "function manager" API. Datum type, PG_FUNCTION_ARGS macro, helpers
-// utils/builtins.h --> helpers for working with built-in types like text, int4, etc
-
-
-// Marks this shared library as a valid PostgreSQL loadable module. Postgres checks for ABI compability
 PG_MODULE_MAGIC;
 
-// Tells PostgreSQL about the function using the V1 calling convention
-PG_FUNCTION_INFO_V1(pg_fk_indexer_hello);
+PG_FUNCTION_INFO_V1(pg_fk_indexer_add);
 
-Datum pg_fk_indexer_hello(PG_FUNCTION_ARGS) {
-    PG_RETURN_TEXT_P(cstring_to_text("hello world from a postgres extension"));
+Datum pg_fk_indexer_add(PG_FUNCTION_ARGS) {
+    int32 a = PG_GETARG_INT32(0);
+    int32 b = PG_GETARG_INT32(1);
+
+    PG_RETURN_INT32(a + b);
 }
