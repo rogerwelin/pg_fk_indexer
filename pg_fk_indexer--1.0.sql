faf6d743dc886838da56cd47e424d2d026947907
roger.welin <roger.welin@icloud.com>
2026-03-02 11:03:52 +0100

    implement hook

--- pg_fk_indexer--1.0.sql
+++ pg_fk_indexer--1.0.sql
@@ -1,4 +1,1 @@
-CREATE FUNCTION pg_fk_indexer_add(a int4, b int4)
-RETURNS int4
-AS 'MODULE_PATHNAME', 'pg_fk_indexer_add'
-LANGUAGE C IMMUTABLE STRICT;
+\echo Use "CREATE EXTENSION pg_fk_indexer" to load this extension. \quit
--- pg_fk_indexer.c
+++ pg_fk_indexer.c
@@ -1,14 +1,37 @@
 #include "postgres.h"
 #include "fmgr.h"
 #include "utils/builtins.h"
+#include "tcop/utility.h" // utility hook
+#include "utils/elog.h"
 
 PG_MODULE_MAGIC;
 
-PG_FUNCTION_INFO_V1(pg_fk_indexer_add);
 
-Datum pg_fk_indexer_add(PG_FUNCTION_ARGS) {
-    int32 a = PG_GETARG_INT32(0);
-    int32 b = PG_GETARG_INT32(1);
+static ProcessUtility_hook_type prev_utility_hook = NULL;
 
-    PG_RETURN_INT32(a + b);
+static void pg_fk_indexer_utility_hook(PlannedStmt *pstmt, const char *queryString,
+                                  bool readOnlyTree, ProcessUtilityContext context,
+                                  ParamListInfo params, QueryEnvironment *queryEnv,
+                                  DestReceiver *dest, QueryCompletion *qc) {
+  elog(NOTICE, "pg_fk_indexer: loaded");
+
+  // Chain to previous hook if one exists, otherwise call the default executor.
+  // Skipping this would swallow the DDL and break other extensions in the hook chain.
+  if (prev_utility_hook) {
+    prev_utility_hook(pstmt, queryString, readOnlyTree, context, params, queryEnv, dest, qc);
+  } else {
+    standard_ProcessUtility(pstmt, queryString, readOnlyTree, context, params, queryEnv, dest, qc);
+  }
+}
+
+// runs when extention is loaded into memory
+void _PG_init(void) {
+  prev_utility_hook = ProcessUtility_hook;
+
+  ProcessUtility_hook = pg_fk_indexer_utility_hook;
+}
+
+// runs when extention is unloaded
+void _PG_fini(void) {
+  ProcessUtility_hook = prev_utility_hook;
 }
