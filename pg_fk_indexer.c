73f10c753cb2b30bd89cd21e0dbc083c73b1c5e8
Roger Welin <roger.welin@icloud.com>
2026-03-02 22:03:12 +0100

    get fk column in createstmt

--- pg_fk_indexer.c
+++ pg_fk_indexer.c
@@ -3,6 +3,8 @@
 #include "utils/builtins.h"
 #include "tcop/utility.h" // utility hook
 #include "nodes/nodes.h"
+#include "nodes/pg_list.h"
+#include "nodes/parsenodes.h"
 #include "utils/elog.h"
 
 PG_MODULE_MAGIC;
@@ -22,6 +24,43 @@ static void pg_fk_indexer_utility_hook(PlannedStmt *pstmt, const char *queryStri
     CreateStmt *stmt = (CreateStmt *) parsetree;
     char *tableName = stmt->relation->relname;
     elog(NOTICE, "pg_fk_indexer: creating table: '%s'", tableName);
+
+    ListCell *cell;
+
+    // loop table elements (columns and constraints)
+    foreach(cell, stmt->tableElts) {
+      Node *element = (Node *) lfirst(cell);
+
+      // table level constraints, eg CREATE TABLE t (a int, b int, FOREIGN KEY (a) REFERENCES other(x));
+      // parser sees this as a standalone Constraint node in the list.
+      if (IsA(element, Constraint))  {
+        Constraint *con = (Constraint *) element;
+        if (con->contype == CONSTR_FOREIGN) {
+          /* Table-level FKs store column names in the 'fk_attrs' list */
+          char *colName = strVal(linitial(con->fk_attrs));
+          elog(NOTICE, "pg_fk_indexer: Found table-level FK on column '%s'", colName);
+        }
+      }
+
+      // Inline Column Constraint, eg CREATE TABLE t (a int REFERENCES other(x));
+      // parser sees a ColumnDef node; the FK is nested inside its 'constraints' list
+      if (IsA(element, ColumnDef)) {
+        ColumnDef *colDef = (ColumnDef *) element;
+        ListCell *colCell;
+
+        foreach(colCell, colDef->constraints) {
+          Node *colElement = (Node *) lfirst(colCell);
+
+          if (IsA(colElement, Constraint)) {
+            Constraint *con = (Constraint *) colElement;
+            if (con->contype == CONSTR_FOREIGN) {
+              /* For inline FKs, the column name is the name of the parent ColumnDef */
+              elog(NOTICE, "pg_fk_indexer: Found inline FK on column '%s'", colDef->colname);
+            }
+          }
+        }
+      }
+    }
   }
 
   if (nodeTag(parsetree) == T_AlterTableStmt) {
